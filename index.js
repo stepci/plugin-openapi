9fd3bfedac61339b3b1448f2f72ce8d72046f409
Mish Ushakov <mishushakov@users.noreply.github.com>
2022-10-07 15:22:39 +0200

    Made body generation optional

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import SwaggerParser from '@apidevtools/swagger-parser'
 const defaultOptions = {
   generator: {
     pathParams: true,
+    body: true,
     optionalParams: true,
     useExampleValues: true,
     useDefaultValues: true
@@ -100,8 +101,7 @@ export async function generateWorkflow (file, {options = defaultOptions}) {
         })
       }
 
-      // Rewrite this
-      if (swagger.paths[path][method].requestBody && (!options.generator.optionalParams ? swagger.paths[path][method].requestBody.required : true)) {
+      if (options.generator.body && swagger.paths[path][method].requestBody && (!options.generator.optionalParams ? swagger.paths[path][method].requestBody.required : true)) {
         const requestBody = swagger.paths[path][method].requestBody.content
 
         for (const contentType in requestBody) {
